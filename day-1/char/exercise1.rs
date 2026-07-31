b15b6904abe66cfa5ec18951ad8638934fe4e6b4
Jubair Hossen Raju <jubairhossenraju@gmail.com>
2026-07-31 12:43:30 +0200

    day-1: Char - exercise 1

---
+++ day-1/char/exercise1.rs
@@ -0,0 +1,8 @@
+use std::mem::size_of_val;
+fn main() {
+    let c1 = 'a';
+    assert_eq!(size_of_val(&c1),4); 
+    let c2 = '中';
+    assert_eq!(size_of_val(&c2),4); 
+    println!("Success!");
+}
