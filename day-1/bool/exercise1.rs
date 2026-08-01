f6991b3de522dbea05c2ba8a53e790776b97ab5e
Jubair Hossen Raju <90641323+jubair211@users.noreply.github.com>
2026-08-01 09:15:21 +0200

    Refactor boolean variable declaration and add comment
    Updated variable declaration for clarity and added comment.

--- day-1/bool/exercise1.rs
+++ day-1/bool/exercise1.rs
@@ -1,6 +1,6 @@
 fn main() {
-    let _f: bool = false;
-    let t = true;
+    let _f: bool = false;  // _f    underscore _ tells Rust: "I'm not going to use this variable" - no warning will be shown
+    let t: bool = true;
     if t {
         println!("Success!");
     }
