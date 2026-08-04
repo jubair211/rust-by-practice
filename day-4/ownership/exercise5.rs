7802f3d122b8a8fadf41c9c01bd5c5bc1fae7377
Jubair Hossen Raju <90641323+jubair211@users.noreply.github.com>
2026-08-04 22:42:27 +0200

    Update variable assignment with Copy semantics

--- day-4/ownership/exercise5.rs
+++ day-4/ownership/exercise5.rs
@@ -11,7 +11,7 @@ fn main() {
 --------- Solution 1 ---------
 
 fn main() {
-    let x: (i32, i32, (), &str) = (1, 2, (), "hello");    
-    let y: (i32, i32, (), &str) = x;
+    let x: (i32, i32, (), &str) = (1, 2, (), "hello");    // Use &str (string literal, is Copy)
+    let y: (i32, i32, (), &str) = x;      // x is automatically copied
     println!("{:?}, {:?}", x, y);
 }
