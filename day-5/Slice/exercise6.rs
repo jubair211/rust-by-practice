f99a3a58d55f1629f740a6a9d7273fa89f14c917
Jubair Hossen Raju <jubairhossenraju@gmail.com>
2026-08-09 23:23:28 +0200

    day-5: Slice  - exercise 6

---
+++ day-5/Slice/exercise6.rs
@@ -0,0 +1,32 @@
+--------- Problem ---------
+
+// Fix errors
+fn main() {
+    let mut s = String::from("hello world");
+
+    // Here, &s is `&String` type, but `first_letter` needs a `&str` type.
+    // It works because `&String` can be implicitly converted to `&str. If you want to know more, this is called `Deref coercion`. 
+    let letter = first_letter(&s);
+
+    s.clear(); // error!
+
+    println!("the first letter is: {}", letter);
+}
+fn first_letter(s: &str) -> &str {
+    &s[..1]
+}
+
+--------- Solution ---------
+
+fn main() {
+    let mut s: String = String::from("hello world");
+    let letter: &str = first_letter(&s);
+    println!("the first letter is: {}", letter);
+    s.clear(); 
+
+    
+}
+fn first_letter(s: &str) -> &str {
+    &s[..1]
+}
+
