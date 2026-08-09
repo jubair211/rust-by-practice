7f6a5cf0054ab890fbf0a3c7b9a40fd1ea8053c4
Jubair Hossen Raju <90641323+jubair211@users.noreply.github.com>
2026-08-09 22:54:42 +0200

    Fix type declaration for s2 in exercise1.rs

--- day-5/Slice/exercise1.rs
+++ day-5/Slice/exercise1.rs
@@ -16,7 +16,7 @@ fn main() {
     let arr: [i32; 3] = [1, 2, 3];
     let s1: &[i32] = &arr[0..2];   // &[1, 2]
 
-    let s2: str = "hello, world" as str;
+    let s2: &str = "hello, world";
 
     println!("Success!");
 }
