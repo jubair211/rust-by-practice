8761205d5d2abfc324ebb0bba179e412979b5402
Jubair Hossen Raju <jubairhossenraju@gmail.com>
2026-08-09 23:32:38 +0200

    day-5: Tuple  - exercise 2

---
+++ day-5/Tuple/exercise2.rs
@@ -0,0 +1,17 @@
+--------- Problem ---------
+
+// Make it work
+fn main() {
+    let t = ("i", "am", "sunface");
+    assert_eq!(t.1, "sunface");
+
+    println!("Success!");
+}
+--------- Solution ---------
+
+fn main() {
+    let t = ("i", "am", "sunface");
+    assert_eq!(t.2, "sunface");
+
+    println!("Success!");
+}
